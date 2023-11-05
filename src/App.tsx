e068a730470ed629865833d2acbfe0338b4b7868
Lonewolfonrock <110655331+Lonewolfonrock@users.noreply.github.com>
2023-11-05 22:59:03 +0545

    last i think

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import data from './Componets/data';
 import NumbersDisplay from './Componets/Calculator'; 
